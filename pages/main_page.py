ce7c056c56d9622adf8b5dccd3c2fab16fcc728c
ValarValar <dan4eg98@mail.ru>
2022-04-19 15:31:38 +0300

    First Main Page Object test added

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,2 +1,8 @@
+from .base_page import BasePage
+from selenium.webdriver.common.by import By
 
 
+class MainPage(BasePage):
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(by=By.CSS_SELECTOR, value="#login_link")
+        login_link.click()
